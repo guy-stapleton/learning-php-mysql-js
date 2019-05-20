2bb9feb43b8905d6430d9f4d99dc03f7de2b22f5
guy.stapleton <guy.stapleton+1@gmail.com>
2019-05-20 16:49:20 +1200

    Added link to functions.php

--- index.php
+++ index.php
@@ -3,85 +3,91 @@
 <html lang="en">
 <meta charset="utf-8">
     <head>
-        <title>[Fill me in]</title>
+        <title>Index Main Page</title>
         <link href="css/styles.css" rel="stylesheet">
     </head>
     <body>
         <div id="app">
             <?php
-                echo "Hello World!<br/>";
-                echo "a: [" . TRUE . "]<br/>";
-                echo "b: [" . FALSE . "]<br/>";
-                $day_number = 134;
+            //     echo "Hello World!<br/>";
+            //     echo "a: [" . TRUE . "]<br/>";
+            //     echo "b: [" . FALSE . "]<br/>";
+            //     $day_number = 134;
 
-                $days_to_new_year = 366 - $day_number;
+            //     $days_to_new_year = 366 - $day_number;
 
-                if ($days_to_new_year < 30) {
-                    echo "It's a long time to new years<br/>";
-                } else {
-                    echo "It's emmienet<br/>";
-                }
+            //     if ($days_to_new_year < 30) {
+            //         echo "It's a long time to new years<br/>";
+            //     } else {
+            //         echo "It's emmienet<br/>";
+            //     }
                 
-                $format = "video";
+            //     $format = "video";
 
-                if ($format == "dvd") {
-                    echo "Get the dvd player ready<br/>";
-                } elseif ($format == "blueray") {
-                    echo "Get out the blueray players";
-                } else {
-                    echo "Hmmmmm....<br/>";
-                }
+            //     if ($format == "dvd") {
+            //         echo "Get the dvd player ready<br/>";
+            //     } elseif ($format == "blueray") {
+            //         echo "Get out the blueray players";
+            //     } else {
+            //         echo "Hmmmmm....<br/>";
+            //     }
 
-                $order = "chicken";
-                echo "So what would you like tonight?<br/>";
-                switch ($order) {
-                    case ("steak"): 
-                        echo "I'll take the steak<br/>";
-                        break;
-                    case ("pork"):
-                        echo "I'll take the pork<br/>";
-                        break;
-                    case ("fish"):
-                        echo "I'll take the fish<br/>";
-                        break;
-                    default:
-                        echo "Hmmmmm....I'll take the chicken. Good old chicken.<br/>";
-                        break;
-                }
+            //     $order = "chicken";
+            //     echo "So what would you like tonight?<br/>";
+            //     switch ($order) {
+            //         case ("steak"): 
+            //             echo "I'll take the steak<br/>";
+            //             break;
+            //         case ("pork"):
+            //             echo "I'll take the pork<br/>";
+            //             break;
+            //         case ("fish"):
+            //             echo "I'll take the fish<br/>";
+            //             break;
+            //         default:
+            //             echo "Hmmmmm....I'll take the chicken. Good old chicken.<br/>";
+            //             break;
+            //     }
 
-                $a;
-                $b = 1;
-                echo "and the results are:<br/>";
-                echo ($a AND $b) . "<br/>";
-                echo ($a OR $b) . "<br/>";
-                echo ($a XOR $b) . "<br/>";
-                echo !$a . "<br/>";
+            //     $a;
+            //     $b = 1;
+            //     echo "and the results are:<br/>";
+            //     echo ($a AND $b) . "<br/>";
+            //     echo ($a OR $b) . "<br/>";
+            //     echo ($a XOR $b) . "<br/>";
+            //     echo !$a . "<br/>";
                 
-                $fuel = 10;
+            //     $fuel = 10;
 
-                while (--$fuel > 1)
-                    echo "Keep driving...<br/>";
-                echo "Out of fuel!<br/>";
+            //     while (--$fuel > 1)
+            //         echo "Keep driving...<br/>";
+            //     echo "Out of fuel!<br/>";
 
-                $truth = ($day_number == 134) ? "Yes" : "No";
-                echo "the truth is $truth";
+            //     $truth = ($day_number == 134) ? "Yes" : "No";
+            //     echo "the truth is $truth";
                 
-                $beastie_boys = array("Mike D", "Adrock", "MCA");
-                echo $beastie_boys[0] . "<br/>";
-                $length = count($beastie_boys);
+            //     $beastie_boys = array("Mike D", "Adrock", "MCA");
+            //     echo $beastie_boys[0] . "<br/>";
+            //     $length = count($beastie_boys);
 
-                for ($i = 0; $i < count($beastie_boys); $i++) {
-                    echo $beastie_boys[$i] . " in the house, watchs gonna do<br/>";
-                    echo "I go AWOL!<br/>";
-                }
-                echo "Hit ti then, then quit it<br/>";
+            //     for ($i = 0; $i < count($beastie_boys); $i++) {
+            //         echo $beastie_boys[$i] . " in the house, watchs gonna do<br/>";
+            //         echo "I go AWOL!<br/>";
+            //     }
+            //     echo "Hit ti then, then quit it<br/>";
 
-                $a = 56;
-                $b = 17;
-                $c = (int)($a / $b);
+            //     $a = 56;
+            //     $b = 17;
+            //     $c = (int)($a / $b);
 
-                echo $c . "<br/>";
-            ?>
+            //     echo $c . "<br/>";
+            // ?> 
+
+            <hr>
+            <h2>Functions and Objects</h2>
+            <hr>
+            <a href="experiment.php">Experiment</a>
+            <a href="functions.php">Functions</a>
         </div>
         <script src="js/index.js"></script>
     </body>
